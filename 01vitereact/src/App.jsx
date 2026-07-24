3baba2e6b768324aa0504b1a6dbca23db9a5f3f8
Ashu <ayushprajapati272007@gmail.com>
2026-07-24 14:18:09 +0530

    Some More concepts

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -3,10 +3,12 @@ import Chai from './chai'
 
 
 function App() {
+  const username = "Ashu"
+
   return (
     <>
       <Chai/>
-      <h1>chai aur React</h1>
+      <h1>chai aur React || {username}</h1>
       <p>test para</p>
     </>
   )
