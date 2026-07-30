586df375eb86575610a7e953fe1f311988a064b0
Ashu <ayushprajapati272007@gmail.com>
2026-07-30 09:20:21 +0530

    Understanding Components

---
+++ ReactBySCS/02components/src/components/Card.jsx
@@ -0,0 +1,10 @@
+function card(){
+    return(
+        <div className="card">
+            <h1>Ashu</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, tenetur.</p>
+        </div>
+    )   
+}
+
+export default card;
---
+++ ReactBySCS/02components/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <div>
+        <div className="nav">
+            This is Nav
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
